19148e4feb1cb9cf5af9486fa8d159b081b6fe5a
Christian Iseli <christian.iseli@epfl.ch>
2023-12-13 00:05:32 +0100

    Add project files.

---
+++ ImageSorter/MainWindow.xaml.cpp
@@ -0,0 +1,77 @@
+// MainWindow.xaml.cpp
+#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+#include "ImageFileInfo.h"
+
+//using namespace winrt;
+//using namespace Microsoft::UI::Xaml;
+namespace winrt
+{
+  using namespace Windows::ApplicationModel;
+  using namespace Windows::Foundation;
+  using namespace Windows::Foundation::Collections;
+  using namespace Windows::Storage;
+  using namespace Windows::Storage::Search;
+  using namespace Microsoft::UI::Xaml;
+  using namespace Microsoft::UI::Xaml::Controls;
+}
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::ImageSorter::implementation
+{
+  IAsyncAction MainWindow::GetItemsAsync()
+  {
+    StorageFolder appInstalledFolder = Package::Current().InstalledLocation();
+    StorageFolder picturesFolder = co_await appInstalledFolder.GetFolderAsync(L"Assets\\Samples");
+
+    auto result = picturesFolder.CreateFileQueryWithOptions(QueryOptions());
+
+    IVectorView<StorageFile> imageFiles = co_await result.GetFilesAsync();
+    for (auto&& file : imageFiles)
+    {
+      Images().Append(co_await LoadImageInfoAsync(file));
+    }
+
+    ImageGridView().ItemsSource(Images());
+  }
+
+  IAsyncOperation<ImageSorter::ImageFileInfo> MainWindow::LoadImageInfoAsync(StorageFile file)
+  {
+    auto properties = co_await file.Properties().GetImagePropertiesAsync();
+    ImageSorter::ImageFileInfo info(properties,
+      file, file.DisplayName(), file.DisplayType());
+    co_return info;
+  }
+
+  void MainWindow::ImageGridView_ContainerContentChanging(ListViewBase const& sender, ContainerContentChangingEventArgs const& args)
+  {
+    if (args.InRecycleQueue())
+    {
+      auto templateRoot = args.ItemContainer().ContentTemplateRoot().try_as<Grid>();
+      auto image = templateRoot.FindName(L"ItemImage").try_as<Image>();
+      image.Source(nullptr);
+    }
+    if (args.Phase() == 0)
+    {
+      args.RegisterUpdateCallback({ this, &MainWindow::ShowImage });
+      args.Handled(true);
+    }
+  }
+
+  fire_and_forget MainWindow::ShowImage(ListViewBase const& sender, ContainerContentChangingEventArgs const& args)
+  {
+    if (args.Phase() == 1)
+    {
+      // It's phase 1, so show this item's image.
+      auto templateRoot = args.ItemContainer().ContentTemplateRoot().try_as<Grid>();
+      auto image = templateRoot.FindName(L"ItemImage").try_as<Image>();
+      auto item = args.Item().try_as<ImageSorter::ImageFileInfo>();
+      image.Source(co_await get_self<ImageSorter::implementation::ImageFileInfo>(item)->GetImageThumbnailAsync());
+    }
+  }
+}
---
+++ ImageSorter/MainWindow.xaml.h
@@ -0,0 +1,47 @@
+// MainWindow.xaml.h
+#pragma once
+
+#include "MainWindow.g.h"
+
+namespace winrt::ImageSorter::implementation
+{
+  struct MainWindow : MainWindowT<MainWindow>
+  {
+    //MainWindow()
+    //{
+        // Xaml objects should not call InitializeComponent during construction.
+        // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
+    //}
+    MainWindow::MainWindow() :
+      m_images(winrt::single_threaded_observable_vector<IInspectable>())
+    {
+      InitializeComponent();
+      GetItemsAsync();
+    }
+
+    Windows::Foundation::Collections::IVector<Windows::Foundation::IInspectable> Images() const
+    {
+      return m_images;
+    }
+
+    void ImageGridView_ContainerContentChanging(
+      Microsoft::UI::Xaml::Controls::ListViewBase const& sender,
+      Microsoft::UI::Xaml::Controls::ContainerContentChangingEventArgs const& args);
+
+  private:
+    Windows::Foundation::Collections::IVector<IInspectable> m_images{ nullptr };
+
+    Windows::Foundation::IAsyncAction GetItemsAsync();
+    Windows::Foundation::IAsyncOperation<ImageSorter::ImageFileInfo> LoadImageInfoAsync(Windows::Storage::StorageFile);
+    fire_and_forget ShowImage(
+      Microsoft::UI::Xaml::Controls::ListViewBase const& sender,
+      Microsoft::UI::Xaml::Controls::ContainerContentChangingEventArgs const& args);
+  };
+}
+
+namespace winrt::ImageSorter::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
